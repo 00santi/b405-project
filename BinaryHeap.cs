af9a69088b28d1ab90dcbf7fbf3f62cda4e9ba4a
00santi <124394899+00santi@users.noreply.github.com>
2026-04-18 16:23:27 -0400

    Add files via upload

---
+++ BinaryHeap.cs
@@ -0,0 +1,54 @@
+﻿namespace B405_Project;
+
+public class BinaryHeap { 
+    List<int> data;
+
+    public BinaryHeap() {
+        data = new List<int>();
+    }
+
+    public void Push(int value) {
+        data.Add(value);
+        BubbleUp(data.Count - 1);
+    }
+
+    void BubbleUp(int i) {
+        while (i > 0) {
+            int parent = (i - 1) / 2;
+            if (data[i] >= data[parent]) 
+                break;
+            (data[i], data[parent]) = (data[parent], data[i]);
+            i = parent;
+        }
+    }
+    
+    public int Peek() {
+        return data[0];
+    }
+
+    public int Pop() {
+       int result = data[0];
+       data[0] =  data[data.Count - 1];
+       data.RemoveAt(data.Count - 1);
+       BubbleDown(0);
+       return result;
+    }
+
+    void BubbleDown(int i) {
+        for (;;) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left >= data.Count) 
+                return;
+            
+            int child = right;
+            if (right >= data.Count || data[left] < data[right])
+                child = left;
+            
+            if (data[child] > data[i])
+                return;
+            (data[i], data[child]) = (data[child], data[i]);
+            i = child;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace B405_Project;
+
+class Program {
+    static void Main(string[] args) {
+        long bh1 = TestBinaryHeap(1_000_000);
+        long fh1 = TestFibHeap(1_000_000);
+        long bh2 = TestBinaryHeap(10_000_000);
+        long fh2 = TestFibHeap(10_000_000);
+        //long bh3 = TestBinaryHeap(100_000_000);
+        //long fb3 = TestFibHeap(100_000_000);
+        Console.WriteLine("N = 1,000,000");
+        Console.WriteLine("Binary Heap: " + bh1 + "ms");
+        Console.WriteLine("Fibonacci Heap: " + fh1 + "ms\n");
+        Console.WriteLine("N = 10,000,000");
+        Console.WriteLine("Binary Heap: " + bh2 + "ms");
+        Console.WriteLine("Fibonacci Heap: " + fh2 + "ms");
+    }
+
+    static long TestBinaryHeap(int N) {
+        var rand = new Random();
+        var sw = new System.Diagnostics.Stopwatch();
+        var heap = new BinaryHeap();
+        
+        sw.Start();
+        for (int i = 0; i < N; i++) 
+            heap.Push(rand.Next());
+        
+        for (int i = 0; i < N; i++)
+            heap.Pop();
+        sw.Stop();
+        
+        return sw.ElapsedMilliseconds;
+    }
+
+    static long TestFibHeap(int N) {
+        var rand = new Random();
+        var sw = new System.Diagnostics.Stopwatch();
+        var heap = new FibHeap();
+        
+        sw.Start();
+        for (int i = 0; i < N; i++)
+            heap.Push(rand.Next());
+
+        for (int i = 0; i < N; i++)
+            heap.Pop();
+        sw.Stop();
+        
+        return sw.ElapsedMilliseconds;
+    }
+}
+
